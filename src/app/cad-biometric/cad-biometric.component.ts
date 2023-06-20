360de781d5c37b84c96d7caf30291e6faa8b6ccb
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2023-06-20 11:20:37 -0300

    Implement new logicals for habilit button 'Enviar'

--- src/app/cad-biometric/cad-biometric.component.ts
+++ src/app/cad-biometric/cad-biometric.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './cad-biometric.component.html',
   styleUrls: ['./cad-biometric.component.css']
 })
+
 export class CadBiometricComponent {
   cadastro: any = {};
   buttonClicked1 = false;
@@ -12,6 +13,7 @@ export class CadBiometricComponent {
   buttonClicked3 = false;
   buttonClicked4 = false;
   modalOpen = false;
+  biometricsCompleted = false;
 
   showModal(buttonNumber: number) {
     const modalElement = document.getElementById('modalBiometric');
@@ -24,15 +26,27 @@ export class CadBiometricComponent {
     switch (buttonNumber) {
       case 1:
         this.buttonClicked1 = true;
+        if (this.buttonClicked2 && this.buttonClicked3 && this.buttonClicked4) {
+          this.biometricsCompleted = true;
+        }
         break;
       case 2:
         this.buttonClicked2 = true;
+        if (this.buttonClicked1 && this.buttonClicked3 && this.buttonClicked4) {
+          this.biometricsCompleted = true;
+        }
         break;
       case 3:
         this.buttonClicked3 = true;
+        if (this.buttonClicked1 && this.buttonClicked2 && this.buttonClicked4) {
+        this.biometricsCompleted = true;
+        }
         break;
       case 4:
         this.buttonClicked4 = true;
+        if (this.buttonClicked2 && this.buttonClicked2 && this.buttonClicked3) {
+          this.biometricsCompleted = true;
+        }
         break;
       default:
         break;
