5cff5585b9bdbd8cb2ed35e415b963d6ace8b026
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2023-06-20 13:08:42 -0300

    Implement new logicals

--- src/app/new-paassenger/new-paassenger.component.ts
+++ src/app/new-paassenger/new-paassenger.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-new-paassenger',
   templateUrl: './new-paassenger.component.html',
   styleUrls: ['./new-paassenger.component.css']
 })
-export class NewPaassengerComponent {
 
+export class NewPaassengerComponent {
+  @ViewChild('formCad') formCad: any;
+  
+  onSubmit(): void {
+    if (this.formCad.valid) {
+      const formData = this.formCad.value;
+      console.log(formData);
+    }
+  }
 }
